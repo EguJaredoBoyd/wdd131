82f196236830f8b702987d3d8bcff7af79679bcd
Victor09041998 <godspowervictor01@gmail.com>
2025-05-24 01:06:20 +0100

    Initial Commit

---
+++ .history/scripts/place_20250524005244.js
@@ -0,0 +1,21 @@
+const temp = parseFloat(document.getElementById("temp").textContent);
+const speed = parseFloat(document.getElementById("speed").textContent);
+const chillSpan = document.getElementById("chill");
+
+function calculateWindChill(t, s){
+    return (35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)).toFixed(1);
+
+}
+
+if (temp <= 50 && speed > 3) {
+    chillSpan.textContent = calculateWindChill(temp, speed) + ' °F';
+} 
+else {
+    chillSpan.textContent = 'N/A';
+}
+
+
+
+
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
